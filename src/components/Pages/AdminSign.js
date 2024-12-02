c36196061e2b01d9c556626cf637b17e6c882e05
GauravKrrr <118409058+gk022135@users.noreply.github.com>
2024-12-02 18:41:52 +0530

    AdminSign.js
    url added bkend

--- src/components/Pages/AdminSign.js
+++ src/components/Pages/AdminSign.js
@@ -32,7 +32,7 @@ export function AdminSign() {
             return handleError('name, email and password are required')
         }
         try {
-            const url = `http://localhost:8080/auth/adminsignup`;
+            const url = `https://qr-bakend.onrender.com`;
 
             const response = await fetch(url, {
                 method: "POST",
